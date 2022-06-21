d0184bfea3e92e56c55eb338eded368cb63de90f
gatwi <faith.murithi@student.moringaschool.com>
2022-06-22 02:47:59 +0300

    commit

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const { expect } = require("chai");
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverseWord = reverse(word);
+
+  return word === reverseWord;
+}
+
+function reverse(word) {
+  // 'abc' = 'cba'
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+  return word.split("").reverse().join("");
 }
 
 /* 
@@ -23,3 +37,21 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+describe("isPalindrome", () => {
+  it("returns true for 'abba'", () => {
+    expect(isPalindrome("abba")).to.be.true;
+  });
+  it("returns true for 'racecar'", () => {
+    expect(isPalindrome("racecar")).to.be.true;
+  });
+  it("returns true for 'a'", () => {
+    expect(isPalindrome("a")).to.be.true;
+  });
+  it("returns false for 'robot'", () => {
+    expect(isPalindrome("robot")).to.be.false;
+  });
+  it("returns false for 'ab'", () => {
+    expect(isPalindrome("ab")).to.be.false;
+  });
+});
